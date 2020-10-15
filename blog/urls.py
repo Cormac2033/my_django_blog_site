dc69f7699ff7dfa1ac8a6758a8724151dc2e3499
Cormac <cormacmcgrath2033@gmail.com>
2020-10-15 12:24:08 -0400

    Add feature to create new blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     # 127.0.0.1.8000 --> local
-    # gra-productions.com --> online
+    # gra-productions.com --> online for example
     path('', views.post_list, name='post_list'),
 
     # 127.0.0.1.8000/post/2 --> local
     # gra-productions.com/post/2 --> online
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    # 127.0.0.1.8000/post/new --> local
+    # gra-productions.com/post/new --> online
+    path('post/new/', views.post_new, name='post_new'),
 ]
